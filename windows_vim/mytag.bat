dae1b5b71c498cbd0c21cd6e6e5613731a9ed175
li6xiang <li6xiang@126.com>
2019-12-09 18:04:01 +0800

    new

--- windows_vim/mytag.bat
+++ windows_vim/mytag.bat
@@ -1,5 +1,5 @@
-del tags cscope.in.out cscope.out cscope.po.out cscope.files
-dir /s /b *.c *.h *.C *.H *.S *.s *.cc *.hh *.cpp> cscope.files
+del tags cscope.in.out cscope.out cscope.po.out cscope.files ncscope.out
+dir /s /b *.c *.h *.s *.cc *.hh *.cpp *cxx > cscope.files
 ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -L cscope.files
 REM  set path=C:\Program Files (x86)\Vim\vim81;E:\mstar\MSC8336\Cygwin\App\Runtime\Cygwin\bin
 cscope -b -k
